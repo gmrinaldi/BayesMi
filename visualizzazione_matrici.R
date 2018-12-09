77e578e1adc8562a22e9399f25b4bc1e82436aaf
CelestePrincipi <principi.celeste@gmail.com>
2018-12-09 11:03:53 +0100

    non so cosa ho di diverso forse ho inizializzato ymean

--- visualizzazione_matrici.R
+++ visualizzazione_matrici.R
@@ -25,3 +25,17 @@ image(as.matrix(log(Y+1)), main='Observed flow',col = rev(heat.colors(200)),  ax
 image(as.matrix(log(ypred1+1)), main='Predicted flow 1', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
 image(as.matrix(log(ypred2+1)), main='Predicted flow 2', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
 image(as.matrix(log(ymean+1)), main='Predicted mean', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+
+load("C:/Users/Celeste/Desktop/netw/BayesMi/grafici_distanza_s_t/previsione2media.RData")
+ymean2<-ymean
+
+abs(ymean-ymean2)
+x11(width=100,height=30)
+par(mfrow=c(1,4))
+image(as.matrix(log(Y+1)), main='Observed flow',col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+image(as.matrix(log(ymean+1)), main='Predicted mean modello3', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+image(as.matrix(log(ymean2+1)), main='Predicted mean modello2', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+image(as.matrix(log(abs(ymean-ymean2)+1)), main='Predicted difference abs(model3-model2)', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+
+range(ymean)
+range(ymean2)
