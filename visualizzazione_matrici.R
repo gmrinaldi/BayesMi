f2081aaf2380c7b8d5bdf9d5e7530d048c1cd270
giulia1995 <giulia0912@gmail.com>
2018-12-12 18:49:04 +0100

    Aggiustato

--- visualizzazione_matrici.R
+++ visualizzazione_matrici.R
@@ -1,14 +1,11 @@
 chains <- rstan::extract(fit.1, permuted = TRUE)
 N<-dim(chains$ypred)[1]
 ypred1<-chains$ypred[1,,]
-<<<<<<< HEAD
-ypred2<-chains$ypred[N,,]
-=======
-ypred2<-chains$ypred[4000,,]
-ymean<-0
->>>>>>> 6457b2ade4197f956a29df0ac20079dac87128ed
 
-ymean=0
+ypred2<-chains$ypred[N,,]
+
+ypred2<-chains$ypred[N,,]
+ymean<-0
 
 for (i in 1:N)
   ymean<-ymean+chains$ypred[i,,]
@@ -29,21 +26,12 @@ mean(colMeans(ypred1))
 #heatmap(as.matrix(adj), keep.dendro=F,Rowv=F, Colv=F)
 x11(width=100,height=30)
 par(mfrow=c(1,4))
-image(as.matrix(log(Y+1)), main='Observed flow',col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(ypred1+1)), main='Predicted flow 1', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(ypred2+1)), main='Predicted flow 2', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(ymean+1)), main='Predicted mean', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+image(as.matrix(log(Y+1)), main='Observed flow',col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y_tot+1)),max(log(Y_tot)),length.out=201) )
+image(as.matrix(log(ypred1+1)), main='Predicted flow 1', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y_tot+1)),max(log(Y_tot)),length.out=201) )
+image(as.matrix(log(ypred2+1)), main='Predicted flow 2', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y_tot+1)),max(log(Y_tot)),length.out=201) )
+image(as.matrix(log(ymean+1)), main='Predicted mean', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y_tot+1)),max(log(Y_tot)),length.out=201) )
 
-load("C:/Users/Celeste/Desktop/netw/BayesMi/grafici_distanza_s_t/previsione2media.RData")
-ymean2<-ymean
-
-abs(ymean-ymean2)
-x11(width=100,height=30)
-par(mfrow=c(1,4))
-image(as.matrix(log(Y+1)), main='Observed flow',col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(ymean+1)), main='Predicted mean modello3', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(ymean2+1)), main='Predicted mean modello2', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
-image(as.matrix(log(abs(ymean-ymean2)+1)), main='Predicted difference abs(model3-model2)', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
+image(as.matrix(log(abs(adj_mattina-adj_pomeriggio)+1)), main='differenza', col = rev(heat.colors(200)),  axes=F, breaks=seq(min(log(Y+1)),max(log(Y)),length.out=201) )
 
 range(ymean)
 range(ymean2)
