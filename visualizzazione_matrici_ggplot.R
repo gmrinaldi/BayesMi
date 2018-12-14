3a2aae195868a1b50c2cfaf792ecc7cd7055d232
giulia1995 <giulia0912@gmail.com>
2018-12-14 09:25:37 +0100

    Ho provato a visuallizzae la matrici con gglot

---
+++ visualizzazione_matrici_ggplot.R
@@ -0,0 +1,13 @@
+library(reshape2)
+library(ggplot2)
+
+Data<-melt(Y)
+Data<-Data[Data!=0,]
+
+ggplot(Data, aes(x = Var2, y = Var1)) + 
+  geom_raster(aes(fill=value)) + 
+  scale_fill_gradient(low="grey90", high="red") +
+  labs(x="letters", y="LETTERS", title="Matrix") +
+  theme_bw() + theme(axis.text.x=element_text(size=9, angle=0, vjust=0.3),
+                     axis.text.y=element_text(size=9),
+                     plot.title=element_text(size=11))
