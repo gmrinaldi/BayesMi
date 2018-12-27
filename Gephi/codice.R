8a74a9444b5400407ee09b799018e45c6f732cd3
gmrinaldi <gianmatteo.rinaldi@mail.polimi.it>
2018-12-27 13:19:39 +0100

    modificato file per gephi

--- Gephi/codice.R
+++ Gephi/codice.R
@@ -1,5 +1,6 @@
 # Rete per gephi
-library(dplyr,tidyr,rgexf)
+library(dplyr,tidyr)
+library(rgexf)
 rete<-bike_NIL_NewDay%>%group_by(StazioneInizio,StazioneFine)%>%summarise(Flow=n())%>%
   ungroup()%>%rename(Source=StazioneInizio,Target=StazioneFine)
 latlong<-read.table("LatLongIdNomeIdnilNomenil_263.txt",head=T,sep=" ")
@@ -36,3 +37,23 @@ geonodes$nome[217]<-"M. Gioia - Regione"
 
 write.gexf(nodes = geonodes,edges = geoedges[,1:2],edgesWeight = geoedges$Flow,
            nodesAtt = geonodes_attr,defaultedgetype="directed",output="C:/Users/nb2/Desktop/Politecnico/StatBayes/BayesMi/Gephi/rete.gexf")
+
+# Rete per gephi (cluster da modello mistura)
+rete<-clustered_flows%>%rename(Source=id_inizio,Target=id_fine)
+
+latlong_staz<-read.table("LatLongIdNomeIdnilNomenil_263.txt",head=T,sep=" ")
+latlong_db<-read.table("db_lat_long.txt",head=T,sep=" ")
+
+latlong_db<-latlong_db%>%rename(nome=db,lat=MeanLat,long=MeanLong)
+
+nodes<-latlong_staz%>%select(id,lat,long)%>%rename(nome=id)%>%filter(nome %in% clustered_flows$inizio_db)%>%
+  bind_rows(latlong_db)%>%mutate(id=1:67)
+
+geonodes<-nodes%>%select(id,nome)
+
+geonodes_attr<-nodes%>%select(lat,long)
+
+geoedges<-rete%>%select(Source,Target,Flow,prediction,cluster)
+  
+write.gexf(nodes = geonodes,edges = geoedges[,1:2],edgesWeight = geoedges$Flow,edgesAtt = geoedges[,4:5],
+           nodesAtt = geonodes_attr,defaultedgetype="directed",output="C:/Users/nb2/Desktop/Politecnico/StatBayes/BayesMi/Gephi/rete_cluster.gexf")
